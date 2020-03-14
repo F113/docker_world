c1eb06a489468d48fa1c903479966074c89ed71e
Borisov <borisov.s.u@yandex.ru>
2020-03-15 01:01:50 +0300

    update

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,7 @@ $host = 'mysql';
 $user = 'root';
 $pass = 'rootpassword';
 $db   = 'world';
+
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
@@ -12,19 +13,20 @@ if ($conn->connect_error) {
 
 $start = microtime(true);
 $result = $conn->query("CALL Step");
-$result = $conn->query("select * from image order by counter desc");
+$result = $conn->query("select * from image order by counter asc");
 $count = $result->num_rows;
 
-$sec = "0";
-//header("Refresh: $sec; url=". $_SERVER['PHP_SELF']);
-echo microtime(true) - $start;
-echo '<br>';
+// echo '<pre>';
+// print_r($_SERVER);
+// echo '</pre>';
+
+if (isset($_GET['refresh']))
+header("Refresh: ".(int)$_GET['refresh']."; url=". $_SERVER['REQUEST_URI']);
+$time_db = microtime(true) - $start;
 
 $html = '';
-
+$start = microtime(true);
 while ($particle = $result->fetch_assoc()) {
-    var_dump($particle);
-    echo '<br>';
     if (!isset($_SESSION['coords'][$particle['id']])) {
         $_SESSION['coords'][$particle['id']] = [
             $_SESSION['coords'][$particle['m1']][0] + (mt_rand(0, 1)*2 - 1),
@@ -33,13 +35,29 @@ while ($particle = $result->fetch_assoc()) {
     }
 
     if ($particle['counter'] < 1) continue;
-    $colors = [255,255,255];
+    $colors = [200-$particle['counter'],200-$particle['counter'],200-$particle['counter']];
 
-    $html .= '<div class="p" style="top:'.$_SESSION['coords'][$particle['id']][0].'px; left:'.$_SESSION['coords'][$particle['id']][1].'px; background-color: rgb('.$colors.')"></div>';
+    $html .= '<div class="p" style="top:'.$_SESSION['coords'][$particle['id']][0].'px; left:'.$_SESSION['coords'][$particle['id']][1].'px; background-color: rgb('.implode(',', $colors).')"></div>';
     //echo implode(' ', $particle) . '<br>';
 }
+$time_php = microtime(true) - $start;
 
-print_r($_SESSION);
+//echo $html;
+//print_r($_SESSION);
 ?>
 
+<style>
+  .p {width: 1px; height:1px; position:absolute;}
+  .center{position:absolute; left:50%; top:50%;}
+  .info {position:absolute; top:0; left:0; width:100%; padding:10px;  background-color: #fffded; border-bottom: 1px solid #ccc;}
+</style>
 
+<div class="info">
+  <div>count: <?=$count?></div>
+  <div>time_db: <?=$time_db?></div>
+  <div>time_php: <?=$time_php?></div>  
+</div>
+
+<div class="center">
+<?=$html?>
+</div>
--- public_html/start.php
+++ public_html/start.php
@@ -9,84 +9,92 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$conn->query('DROP DATABASE world');
-$conn->query('CREATE DATABASE world');
-$conn->query('USE world');
+try {
+  $conn->query('DROP DATABASE world');
+  $conn->query('CREATE DATABASE world');
+  $conn->query('USE world');
 
-$conn->query("
-CREATE TABLE particles (
-    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    m1 INT(11) NOT NULL,
-    m2 INT(11) NOT NULL,
-    new INT(11) NOT NULL DEFAULT '0',
-    `counter` INT(11) NOT NULL DEFAULT '0'
-) ENGINE=MEMORY
-");
+  $conn->query("
+  CREATE TABLE particles (
+      id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      m1 INT(11) NOT NULL,
+      m2 INT(11) NOT NULL,
+      new INT(11) NOT NULL DEFAULT '0',
+      `counter` INT(11) NOT NULL DEFAULT '0'
+  ) ENGINE=MEMORY
+  ");
 
-//print_r($conn->error_list);
+  //print_r($conn->error_list);
 
-$conn->query("
-CREATE TABLE `image` (
-    `id` INT(11) NOT NULL,
-    `m1` INT(11) NOT NULL,
-    `counter` INT(11) NOT NULL DEFAULT '0'
-) ENGINE=MEMORY;
-");
+  $conn->query("
+  CREATE TABLE `image` (
+      `id` INT(11) NOT NULL,
+      `m1` INT(11) NOT NULL,
+      `counter` INT(11) NOT NULL DEFAULT '0'
+  ) ENGINE=MEMORY;
+  ");
 
-$conn->query("INSERT INTO particles (id, m1, m2, new, counter) VALUES (1, 2, 0, 0, 0);");
-$conn->query("INSERT INTO particles (id, m1, m2, new, counter) VALUES (2, 1, 0, 0, 0);");
+  $conn->query("INSERT INTO particles (id, m1, m2, new, counter) VALUES (1, 2, 0, 0, 0);");
+  $conn->query("INSERT INTO particles (id, m1, m2, new, counter) VALUES (2, 1, 0, 0, 0);");
 
-$conn->query("
-CREATE PROCEDURE Step()
-BEGIN
-    DECLARE _id INT DEFAULT NULL;
-    DECLARE _m1 INT;
-    DECLARE _m2 INT;
-    DECLARE _new INT;
+  $conn->query("
+  CREATE PROCEDURE Step()
+  BEGIN
+      DECLARE _id INT DEFAULT NULL;
+      DECLARE _m1 INT;
+      DECLARE _m2 INT;
+      DECLARE _new INT;
 
-    REPEAT
-        SET @_id = NULL;
-        SELECT id, m1, m2 INTO @_id, @_m1, @_m2 FROM particles WHERE `new` = 0 LIMIT 1;
+      REPEAT
+          SET @_id = NULL;
+          SELECT id, m1, m2 INTO @_id, @_m1, @_m2 FROM particles WHERE `new` = 0 LIMIT 1;
 
-        SELECT m1 INTO @_new FROM particles WHERE id = @_m1;
-        UPDATE particles SET counter = counter+1 WHERE id = @_m1;
-        IF (@_new = @_id OR @_new = @_m1 OR @_new = @_m2) THEN
-            INSERT INTO particles (m1,m2,new,counter) VALUES (@_id,0,@_m1,0);
-            UPDATE particles SET new=LAST_INSERT_ID() WHERE id = @_id;
-        ELSE
-            UPDATE particles SET new=@_new WHERE id = @_id;
-        END IF;
-    UNTIL (@_id IS NULL)
-    END REPEAT;
-    
-    CALL Shift();
-END
-");
+          SELECT m1 INTO @_new FROM particles WHERE id = @_m1;
+          UPDATE particles SET counter = counter+1 WHERE id = @_m1;
+          IF (@_new = @_id OR @_new = @_m1 OR @_new = @_m2) THEN
+              INSERT INTO particles (m1,m2,new,counter) VALUES (@_id,0,@_m1,0);
+              UPDATE particles SET new=LAST_INSERT_ID() WHERE id = @_id;
+          ELSE
+              UPDATE particles SET new=@_new WHERE id = @_id;
+          END IF;
+      UNTIL (@_id IS NULL)
+      END REPEAT;
 
-$conn->query("
-CREATE PROCEDURE Shift()
-BEGIN
-    TRUNCATE image;
-    INSERT INTO image (id, m1, counter) SELECT id, m1, counter FROM particles;
-    UPDATE particles SET m2 = m1, m1 = new, counter = 0, new = 0;
-END
-");
+      CALL Shift();
+  END
+  ");
 
-$conn->query("
-CREATE PROCEDURE Clear()
-BEGIN
-    TRUNCATE image;
-    TRUNCATE particles;
-    INSERT INTO particles (id, m1, m2, new, counter) VALUES (1, 2, 0, 0, 0);
-    INSERT INTO particles (id, m1, m2, new, counter) VALUES (2, 1, 0, 0, 0);
-END
-");
+  $conn->query("
+  CREATE PROCEDURE Shift()
+  BEGIN
+      TRUNCATE image;
+      INSERT INTO image (id, m1, counter) SELECT id, m1, counter FROM particles;
+      UPDATE particles SET m2 = m1, m1 = new, counter = 0, new = 0;
+  END
+  ");
+
+  $conn->query("
+  CREATE PROCEDURE Clear()
+  BEGIN
+      TRUNCATE image;
+      TRUNCATE particles;
+      INSERT INTO particles (id, m1, m2, new, counter) VALUES (1, 2, 0, 0, 0);
+      INSERT INTO particles (id, m1, m2, new, counter) VALUES (2, 1, 0, 0, 0);
+  END
+  ");
+
+} catch (Exception $e) {
+  var_dump($e);
+}
 
 if ($conn->error_list === []) {
     echo 'World restarted';
+    $_SESSION['coords'] = [];
     $_SESSION['coords'][1] = [-1, 1];
     $_SESSION['coords'][2] = [1, -1];
+    echo '<pre>';
     print_r($_SESSION);
+    echo '</pre>';
 } else {
     print_r($conn->error_list);
 }
